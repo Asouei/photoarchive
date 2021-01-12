a933bba75b57b85d9e9f10781aa5b0195a0c62e4
Alex Mikhailishin <asouei.hax@gmail.com>
2021-01-13 01:53:06 +0300

    Фотки она таки грузит

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import datetime
 import time
 import json
-
+from alive_progress import alive_bar
 
 TOKEN = ''
 NAME = ''
@@ -105,8 +105,40 @@ class User():
             final_json = json.dumps(final_list)
             f.write(final_json)
 
+        print()
+        print()
+        token_yd = input('Введите OAuth токен: ')
+        print()
+        print('----начинаю выгрузку на Yandex диск----')
+        print()
 
 
+
+
+        url_path = 'Vk_Archive'
+
+        response_put = requests.put('https://cloud-api.yandex.net/v1/disk/resources',
+                                    headers={'Authorization': token_yd}, params={'path': url_path})
+
+        with alive_bar(len(final_list)) as bar:
+            for items in final_list:
+
+                final_urlpath = (f'{url_path}_{items}["path"]')
+                final_urlpath = items["path"]
+
+                response = requests.get('https://cloud-api.yandex.net/v1/disk/resources/upload',
+                                        headers={'Authorization': token_yd}, params={'path': final_urlpath})
+                # print(response.status_code)
+                # pprint(response.json())
+                upload_url = response.json()['href']
+
+                with open(items['path'], 'rb') as f:
+                    resp = requests.put(upload_url, files={'file': f})
+                print()
+                bar()
+                time.sleep(1)
+
+        print('загружено')
 
 
 
@@ -125,6 +157,8 @@ def intro():
     print("      v. 1.1                                                    by Alex Mikhailishin")
     print()
     print("_________________________________Список комманд_____________________________________")
+
+
 
 def token_config():
     while True:
@@ -210,39 +244,5 @@ main()
 
 
 
-#
-# # Print iterations progress
-# def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-#     """
-#     Call in a loop to create terminal progress bar
-#     @params:
-#         iteration   - Required  : current iteration (Int)
-#         total       - Required  : total iterations (Int)
-#         prefix      - Optional  : prefix string (Str)
-#         suffix      - Optional  : suffix string (Str)
-#         decimals    - Optional  : positive number of decimals in percent complete (Int)
-#         length      - Optional  : character length of bar (Int)
-#         fill        - Optional  : bar fill character (Str)
-#         printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-#     """
-#     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-#     filledLength = int(length * iteration // total)
-#     bar = fill * filledLength + '-' * (length - filledLength)
-#     print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-#     # Print New Line on Complete
-#     if iteration == total:
-#         print()
-#
-# import time
-#
-# # A List of Items
-# items = list(range(0, 57))
-# l = len(items)
-#
-# # Initial call to print 0% progress
-# printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-# for i, item in enumerate(items):
-#     # Do stuff...
-#     time.sleep(0.1)
-#     # Update Progress Bar
-#     printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+
